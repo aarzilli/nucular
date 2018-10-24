8231df9c2afb107bc2c1205cb17b36a2bd12a63d
aarzilli <alessandro.arzilli@gmail.com>
2018-10-24 10:04:03 +0200

    simple example

---
+++ _examples/simple/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aarzilli/nucular"
+	"github.com/aarzilli/nucular/style"
+)
+
+var count int
+
+func main() {
+	wnd := nucular.NewMasterWindow(0, "Counter", updatefn)
+	wnd.SetStyle(style.FromTheme(style.DarkTheme, 2.0))
+	wnd.Main()
+}
+
+func updatefn(w *nucular.Window) {
+	w.Row(50).Dynamic(1)
+	if w.ButtonText(fmt.Sprintf("increment: %d", count)) {
+		count++
+	}
+}
