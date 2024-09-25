602a17fde461563d290dfb8825124786bb2693b1
aarzilli <alessandro.arzilli@gmail.com>
2024-09-25 19:20:08 +0200

    adds ActivateWindow method to MasterWindow
    Adds ActivateWindow method to MasterWindow to raise a specific window.

--- context.go
+++ context.go
@@ -220,8 +220,8 @@ func (ctx *context) Reset() {
 
 func (ctx *context) Restack() {
 	defer func() {
-		if ctx.Input.activateEditorWindow != nil {
-			ctx.Input.activateEditorWindow = nil
+		if ctx.Input.activateWindow != nil {
+			ctx.Input.activateWindow = nil
 		} else {
 			ctx.Input.activateEditor = nil
 		}
@@ -233,7 +233,7 @@ func (ctx *context) Restack() {
 			break
 		}
 	}
-	if !clicked && ctx.Input.activateEditorWindow == nil {
+	if !clicked && ctx.Input.activateWindow == nil {
 		return
 	}
 	ctx.dockedWindowFocus = 0
@@ -257,7 +257,7 @@ func (ctx *context) Restack() {
 		if ctx.Windows[i].flags&windowTooltip != 0 {
 			continue
 		}
-		if ctx.restackClick(ctx.Windows[i]) || ctx.Input.activateEditorWindow == ctx.Windows[i] {
+		if ctx.restackClick(ctx.Windows[i]) || ctx.Input.activateWindow == ctx.Windows[i] {
 			found = true
 			if toplevelIdx != i {
 				newToplevel := ctx.Windows[i]
@@ -280,7 +280,7 @@ func (ctx *context) Restack() {
 		if ctx.restackClick(w) && (w.flags&windowDocked != 0) {
 			ctx.dockedWindowFocus = w.idx
 		}
-		if ctx.Input.activateEditorWindow == w {
+		if ctx.Input.activateWindow == w {
 			ctx.dockedWindowFocus = w.idx
 		}
 		return w
@@ -337,6 +337,9 @@ func (ctx *context) Walk(fn WindowWalkFn) {
 
 func (ctx *context) restackClick(w *Window) bool {
 	if !ctx.Input.Mouse.valid {
+		return false
+	}
+	if ctx.Input.activateWindow != nil {
 		return false
 	}
 	for _, b := range []mouse.Button{mouse.ButtonLeft, mouse.ButtonRight, mouse.ButtonMiddle} {
